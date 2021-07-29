536577ee486dd99e461d2dda61bcda0b9f09f813
Shiv Kumar <helloshivmca@gmail.com>
2021-07-29 17:56:41 +0530

    Completed!!!

---
+++ 06_SocialBook/js/script.js
@@ -0,0 +1,8 @@
+var settingsmenu = document.querySelector(".settings_menu");
+
+console.log(settingsmenu)
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings_menu_height")
+
+}
